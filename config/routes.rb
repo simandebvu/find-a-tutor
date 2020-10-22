17b259b9a65d206e70a6cf0ab0dea62d754cfb22
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-22 23:25:10 +0200

    feat: add namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      get 'tutors/index'
+      post 'tutors/create'
+      get '/show/:id', to: 'tutors#show'
+      delete '/destroy/:id', to: 'tutors#destroy'
+    end
+  end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
