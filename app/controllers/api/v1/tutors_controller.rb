47beb08ee379e76e9f8543003bcc4a3569f44603
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-22 23:17:20 +0200

    feat: add tutors controller

---
+++ app/controllers/api/v1/tutors_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::TutorsController < ApplicationController
+  def index
+    tutor = Tutor.all.order(created_at: :desc)
+    render json: tutor
+  end
+
+  def create
+    tutor = Tutor.create!(tutor_params)
+    if tutor
+      render json: tutor
+    else
+      render json: tutor.errors
+    end
+  end
+
+  def show
+    if tutor
+      render json: tutor
+    else
+      render json: tutor.errors
+    end
+  end
+
+  def destroy
+    tutor&.destroy
+    render json: { message: 'Sucessfully Removed Tutor'}
+  end
+
+  private 
+
+  def tutor_params
+    params.permit(:name, :links, :description, :image)
+  end
+
+  def tutor
+    @tutor ||= Tutor.find(params[:id])    
+  end
+end
