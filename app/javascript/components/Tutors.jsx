48c518a329f4189bc67981207eb8031b050d6aa6
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-27 00:14:53 +0200

    feat: refactor tutors

--- app/javascript/components/Tutors.jsx
+++ app/javascript/components/Tutors.jsx
@@ -33,7 +33,7 @@ class Tutors extends React.Component{
                     <div className="card-body">
                         <h5 className="card-title">{tutor.name}</h5>
                         <Link to={`/tutor/${tutor.id}`} className="btn custom-button">
-                        View Recipe
+                        View Profile
                         </Link>
                     </div>
                 </div>
@@ -42,26 +42,16 @@ class Tutors extends React.Component{
         const noTutor = (
             <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
               <h4>
-                No tutors yet. Why not <Link to="/new_tuto">add one</Link>
+                No tutors yet. Why not <Link to="/new_tutor">add one?</Link>
               </h4>
             </div>
         );
         return (
             <>
-              <section className="jumbotron jumbotron-fluid text-center">
-                <div className="container py-5">
-                  <h1 className="display-4">Tutors for every occasion</h1>
-                  <p className="lead text-muted">
-                    We’ve pulled together our most popular recipes, our latest
-                    additions, and our editor’s picks, so there’s sure to be something
-                    tempting for you to try.
-                  </p>
-                </div>
-              </section>
               <div className="py-5">
                 <main className="container">
                   <div className="text-right mb-3">
-                    <Link to="/recipe" className="btn custom-button">
+                    <Link to="/new_tutor" className="btn custom-button">
                       Create New Tutor
                     </Link>
                   </div>
