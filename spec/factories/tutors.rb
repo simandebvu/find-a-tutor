e0355a3923000732e9f430897685de335b012137
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-27 19:03:43 +0200

    tests: tie tutors created in tests to user

--- spec/factories/tutors.rb
+++ spec/factories/tutors.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
+  user = User.new
+  user.email = 'bender@mail.com'
+  user.password = "awesomepassword"
+  user.password_confirmation = "awesomepassword"
+  user.save
   factory :tutor do
+    user { user }
     name { Faker::Name.name }
     description { Faker::Company.name }
     image { 'noneimage' }
